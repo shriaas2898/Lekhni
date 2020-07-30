167f448005f8e16172f539dda59d5bd8c47db072
shriaas2898 <aasthashrivastava410@gmail.com>
2020-07-30 16:59:02 +0530

    Added page for logon

---
+++ login.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+$_SESSION[user_id] = -1;
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Lekhni SignUp</title>
+  </head>
+  <body>
+
+    <div class="header">
+      <a href="index.php" class="logo"> <img src="files/logo.png" alt="Lekhni" height="150"> </a>
+      <div class="header-right">
+        <a  href="index.php">Home</a>
+        <a  href="register.php">Sign Up</a>
+        <a class="active" href="#">Sign In</a>
+
+      </div>
+    </div>
+
+
+  <form action="login.php" method="post" style="border:1px solid #ccc; margin:10px 100px 10px 100px;">
+    <div class="container">
+    <h1>Sign In</h1>
+    <hr>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="pass"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="pass" required>
+
+    <div class="clearfix">
+      <button type="submit" class="signupbtn" name="submit">Sign In</button>
+    </div>
+  </div>
+  </form>
+  </body>
+  <?php
+    if(isset($_POST["submit"])){
+      print_r($_POST);
+    //If passwords are not matching
+    /*if($_POST["pass"] != $_POST["pass-repeat"]){
+      die("Passwords do not match")
+    }*/
+    try{
+          //Create connection
+          $conn = new mysqli("localhost","pma","pass","lekhni_db");
+          // set the PDO error mode to exception
+          //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          //Check connection
+          if($conn->connect_error){
+            die("Connection failed: " . $conn->connect_error);
+          }
+          $email = $_POST["email"];
+          $user_pass = $_POST['pass'];
+
+          $result = $conn->query("SELECT uid, pass FROM user WHERE email = '$email' LIMIT 1");
+          $row = $result->fetch_assoc();
+          $stored_pass =$row["pass"];
+          $id = $row["uid"];
+          if (password_verify($user_pass,$stored_pass)){
+
+            $_SESSION["user_id"] = $id;
+            header("Location: index.php");
+            echo "<script type='text/javascript'>alert('You have succsessfully registered!');</script>";
+            die("Login succsessful");
+          }
+
+
+  } catch(Exception $e) {
+    //Display message on unsuccsessful registration
+    echo "Error: " . $e->getMessage();
+    //echo "<script type='text/javascript'>alert('We are not able to complete your registration, please try again later.');</script>";
+  }
+}
+   ?>
+</html>
---
+++ register.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Lekhni SignUp</title>
+  </head>
+  <body>
+
+    <div class="header">
+      <a href="index.php" class="logo"> <img src="files/logo.png" alt="Lekhni" height="150"> </a>
+      <div class="header-right">
+        <a  href="index.php">Home</a>
+        <a class="active" href="#default">Sign Up</a>
+        <a href="login.php">Sign In</a>
+
+      </div>
+    </div>
+
+
+  <form action="register.php" method="post" style="border:1px solid #ccc; margin:10px 100px 10px 100px;">
+    <div class="container">
+    <h1>Sign Up</h1>
+    <p>Please fill in this form to create an account.</p>
+    <hr>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="name"><b>Name</b></label>
+    <input type="text" placeholder="Enter Display Name" name="name" required>
+
+    <label for="pass"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="pass" required>
+
+    <!--<label for="pass-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>-->
+
+
+    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+    <div class="clearfix">
+      <a href="index.php"><button type="button" class="cancelbtn">Cancel</button></a>
+      <button type="submit" class="signupbtn" name="submit">Sign Up</button>
+    </div>
+  </div>
+  </form>
+  </body>
+  <?php
+    if(isset($_POST["submit"])){
+      print_r($_POST);
+    //If passwords are not matching
+    /*if($_POST["pass"] != $_POST["pass-repeat"]){
+      die("Passwords do not match")
+    }*/
+    try{
+          //Create connection
+          $conn = new mysqli("localhost","pma","pass","lekhni_db");
+          // set the PDO error mode to exception
+          //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          //Check connection
+          if($conn->connect_error){
+            die("Connection failed: " . $conn->connect_error);
+          }
+
+          $stmnt = $conn->prepare("INSERT INTO user (email,name,pass) values (?, ?, ?)");
+          $stmnt->bind_param("sss", $email, $name ,$hashed_pass);
+
+          $email = $_POST["email"];
+          $name = $_POST["name"];
+          $hashed_pass = password_hash($_POST['pass'], PASSWORD_BCRYPT);
+
+          $stmnt->execute();
+          //Display message on succsessful registration
+          echo "<script type='text/javascript'>alert('You have succsessfully registered!');</script>";
+          header("Location: login.php");
+
+  } catch(Exception $e) {
+    //Display message on unsuccsessful registration
+    echo "Error: " . $e->getMessage();
+    //echo "<script type='text/javascript'>alert('We are not able to complete your registration, please try again later.');</script>";
+  }
+}
+   ?>
+</html>
